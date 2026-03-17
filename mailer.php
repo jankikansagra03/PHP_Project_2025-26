dda420e8ef8c59c303c3d629532c6361604cf7de
jankikansagra03 <122074631+jankikansagra03@users.noreply.github.com>
2026-03-17 10:33:11 +0530

    chnages

--- mailer.php
+++ mailer.php
@@ -34,7 +34,7 @@ function sendEmail($to, $subject, $body, $file = null)
         $mail->Host       = 'smtp.gmail.com';
         $mail->Port       = 465;
         $mail->Username   = 'kansagrajanki@gmail.com';
-        $mail->Password   = 'xvjf ojlt whgy dxib';   // <-- Add your Gmail App Password here
+        $mail->Password   = '';   // <-- Add your Gmail App Password here
 
         // Disable SSL peer verification (for local/dev environments)
         $mail->SMTPOptions = [
